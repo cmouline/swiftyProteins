1f9a1b68da47419ab08d5a89536648611e68903c
Chloe MOULINET <cmouline@e1r11p4.42.fr>
2017-02-09 10:55:42 +0100

    Getting html content from url + split into arrays

--- swiftyProteins/swiftyProteins/ViewController.swift
+++ swiftyProteins/swiftyProteins/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func parseHTML() {
+        
+        let myURLString = "https://files.rcsb.org/ligands/view/V11_ideal.pdb"
+        
+        guard let myURL = URL(string: myURLString) else {
+            print("Error: \(myURLString) doesn't seem to be a valid URL")
+            return
+        }
+        
+        do {
+            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+            let htmlContent = myHTMLString.characters.split(separator: "\n").map(String.init)
+            for line in htmlContent {
+                let elem = line.characters.split(separator: " ").map(String.init)
+                if elem[0] == "ATOM" {
+                    print("ATOM : \(elem)")
+                } else if elem[0] == "CONECT" {
+                    print("CONECT : \(elem)")
+                }
+            }
+            //            print("HTML : \()")
+        } catch let error {
+            print("Error: \(error)")
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        parseHTML()
+        
     }
 
     override func didReceiveMemoryWarning() {
