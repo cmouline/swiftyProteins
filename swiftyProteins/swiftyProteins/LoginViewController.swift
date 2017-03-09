0d4c85ec7119a90ca85607301828ed8b049d6722
Chloe MOULINET <cmouline@e1r10p4.42.fr>
2017-03-09 12:47:45 +0100

    Add touchID error messages

--- swiftyProteins/swiftyProteins/LoginViewController.swift
+++ swiftyProteins/swiftyProteins/LoginViewController.swift
@@ -43,19 +43,59 @@ class LoginViewController: UIViewController {
         let reason = "Identify yourself!"
         
         context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
-            [unowned self] success, authenticationError in
-            
+            [unowned self] success, error in
+
             DispatchQueue.main.async {
+            
                 if success {
-//                        self.runSecretCode()
+                    
                     self.performSegue(withIdentifier: "showLigandsList", sender: self)
+                    
                 } else {
-                    let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
-                    ac.addAction(UIAlertAction(title: "OK", style: .default))
-                    ac.view.tintColor = UIColor.red // change text color of the buttons
-                    ac.view.backgroundColor = UIColor.red  // change background color
-                    ac.view.layer.cornerRadius = 25   // change corner radius
-                    self.present(ac, animated: true)
+                
+                    if let errorObj = error as? NSError {
+                        print("error: \(errorObj)")
+                        print("error: \(errorObj.code)")
+                        var message = "Touch ID may not be configured"
+                        
+                        switch errorObj.code {
+                        case LAError.authenticationFailed.rawValue :
+                            message = "There was a problem verifying your identity."
+                        case LAError.userCancel.rawValue:
+                            message = "Authentication canceled."
+                            // Fallback button was pressed and an extra login step should be implemented for iOS 8 users.
+                        // By the other hand, iOS 9+ users will use the pasccode verification implemented by the own system.
+                        case LAError.userFallback.rawValue:
+                            message = "You can only identify yourself with Touch ID."
+                        case LAError.systemCancel.rawValue:
+                            message = "An error happened and authentication was canceled by the system. Please try again."
+                        case LAError.passcodeNotSet.rawValue:
+                            message = "Passcode is not set on the device."
+                        case LAError.touchIDNotAvailable.rawValue:
+                            message = "Touch ID is not available on the device."
+                        case LAError.touchIDNotEnrolled.rawValue:
+                            message = "Touch ID has no enrolled fingers."
+                        // iOS 9+ functions
+                        case LAError.touchIDLockout.rawValue:
+                            message = "There were too many failed Touch ID attempts and Touch ID is now locked."
+                        case LAError.appCancel.rawValue:
+                            message = "Authentication was canceled by application."
+                        case LAError.invalidContext.rawValue:
+                            message = "LAContext passed to this call has been previously invalidated."
+                        // MARK: IMPORTANT: There are more error states, take a look into the LAError struct
+                        default:
+                            message = "Touch ID may not be configured"
+                            break
+                        }
+                     
+                        let ac = UIAlertController(title: "Authentication failed", message: message, preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        ac.view.tintColor = UIColor.red // change text color of the buttons
+                        ac.view.backgroundColor = UIColor.red  // change background color
+                        ac.view.layer.cornerRadius = 25   // change corner radius
+                        self.present(ac, animated: true)
+                        
+                    }
                 }
             }
         }
